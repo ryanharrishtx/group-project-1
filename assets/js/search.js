fd2eda044ea63d3ec9f2b1631e9b4e7c1134f744
ryanharrishtx@gmail.com <ryanharrishtx@gmail.com>
2023-10-03 20:34:56 -0500

    pushing so others can help solve issue

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,10 +1,4 @@
-// API LINKS:
-// Cocktail Database:
-// https://rapidapi.com/thecocktaildb/api/the-cocktail-db/
-// Wiki API:
-// https://www.mediawiki.org/wiki/API:Main_page
-
-
+// search.js
 
 let cocktailDBAPIKey = "000769ddcamsh5836fbe556e139ap100c26jsnaf07c3af13fa";
 let liquorOptions = ["Vodka", "Gin", "Whiskey", "Tequila", "Rum"];
@@ -15,20 +9,18 @@ document.addEventListener("DOMContentLoaded", () => {
     function createButton() {
         for (let i = 0; i < liquorOptions.length; i++) { 
             let button = document.createElement("button");
-            button.setAttribute("id", "value-button");
+            button.setAttribute("class", "value-button"); // Use class instead of ID
             button.setAttribute("type", "button");
             button.setAttribute("value", liquorOptions[i]);
             button.textContent = liquorOptions[i].toUpperCase();
             container.appendChild(button);
-
-            console.log(button);
         }
-    };
-createButton();
+    }
 
-    let valueButton = document.querySelectorAll("button");
-    valueButton.forEach((button) => {
-        button.addEventListener("click", (event) => {
+    createButton();
+
+    container.addEventListener("click", (event) => {
+        if (event.target.classList.contains("value-button")) {
             document.getElementById("input-container").style.display = "none";
             const buttonValue = event.target.getAttribute("value").toLowerCase();
             fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${buttonValue}`)
@@ -41,24 +33,30 @@ createButton();
                 let drinkList = document.createElement("div");
                 drinkList.setAttribute("id", "drink-list");
                 drinkContainer.appendChild(drinkList);
+
                 for (let i = 0; i < data.drinks.length; i++) {
-                    let drinkListItem = document.createElement("card");
-                    drinkListItem.setAttribute("id", "drink-list-item");
-                    drinkListItem.setAttribute("class", "card");
+                    let drinkListItem = document.createElement("div"); // Changed "card" to "div"
+                    drinkListItem.setAttribute("class", "drink-list-item card"); // Add card class
                     drinkListItem.setAttribute("style", "width: 18rem;");
                     let drinkImage = document.createElement("img");
                     drinkImage.setAttribute("src", data.drinks[i].strDrinkThumb);
                     let drinkImageSrc = drinkImage.getAttribute("src");
                     console.log(drinkImageSrc);
                     let drinkName = data.drinks[i].strDrink.toUpperCase();
-                    drinkListItem.innerHTML = `<img src="${drinkImageSrc}" /> ${drinkName}`;
+                    let recipeButton = document.createElement("button");
+                    recipeButton.setAttribute("class", "recipe-button"); // Use class instead of ID
+                    recipeButton.setAttribute("type", "button");
+                    recipeButton.setAttribute("value", data.drinks[i].idDrink); // Use the drink ID as value
+                    recipeButton.textContent = "RECIPE";
+                    drinkListItem.innerHTML = `<img src="${drinkImageSrc}" /> ${drinkName} ${recipeButton.outerHTML}`;
                     drinkList.appendChild(drinkListItem);
-                    console.log(drinkListItem);
+
+                    // Moved the event listener outside the loop
                 }
             })
             .catch((error) => {
                 console.log(error);
-            })
-        })
-    })
+            });
+        }
+    });
 });
