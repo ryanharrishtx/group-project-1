e428d323456c79654fc53d36dcd579c19356dbea
gracecatk <gracie.kiker@gmail.com>
2023-10-03 20:55:46 -0500

    fixed error on line 6 & 22 in recipes js

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const resultsContainer = document.getElementById("results-container");
 
     resultsContainer.addEventListener("click", async (event) => {
+        event.preventDefault(); 
         if (event.target.classList.contains("recipe-button")) {
             const clickedDrinkId = event.target.getAttribute("value");
             const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${clickedDrinkId}`;
@@ -12,11 +13,13 @@ document.addEventListener("DOMContentLoaded", () => {
                 const response = await fetch(endpoint);
                 const data = await response.json();
                 console.log(data);
-                // Handle the recipe data here, e.g., display it on the page.
-                window.location.replace("./results.html");
-                const resultEl = document.querySelector("#results");
+
+                const resultEl = document.querySelector("#results-container");
+                console.log(resultEl, "Result Element")
                 if (resultEl) {
                     resultEl.innerHTML = data.drinks[0].strInstructions;
+                    const recipeData = encodeURIComponent(data.drinks[0].strInstructions);
+                    window.location.href = `results.html`;
                 } else {
                     console.error("No results element found");
                 }
@@ -26,6 +29,3 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
-
-// const recipeData = encodeURIComponent(data.drinks[0].strInstructions);
-//                 window.location.href = `recipe.html?recipe=${recipeData}`;
--- assets/js/results.js
+++ assets/js/results.js
@@ -3,3 +3,4 @@
 // https://rapidapi.com/thecocktaildb/api/the-cocktail-db/
 // Wiki API:
 // https://www.mediawiki.org/wiki/API:Main_page
+
