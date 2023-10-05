9f661107af7d862fe6e6e888fa60cabb8d1e7a74
ryanharrishtx <139286619+ryanharrishtx@users.noreply.github.com>
2023-10-04 21:03:37 -0500

    stashing

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -1,9 +1,11 @@
+recipes.js
 // recipes.js
 
 document.addEventListener("DOMContentLoaded", () => {
     const resultsContainer = document.getElementById("results-container");
 
     resultsContainer.addEventListener("click", async (event) => {
+        event.preventDefault(); 
         if (event.target.classList.contains("recipe-button")) {
             const clickedDrinkId = event.target.getAttribute("value");
             const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${clickedDrinkId}`;
@@ -12,11 +14,15 @@ document.addEventListener("DOMContentLoaded", () => {
                 const response = await fetch(endpoint);
                 const data = await response.json();
                 console.log(data);
-                // Handle the recipe data here, e.g., display it on the page.
-                window.location.replace("./results.html");
-                const resultEl = document.querySelector("#results");
+                let jsonData = localStorage.setItem("recipe", JSON.stringify(data.drinks[0]));
+                console.log(jsonData);
+
+                const resultEl = document.querySelector("#results-container");
+                console.log(resultEl, "Result Element")
                 if (resultEl) {
                     resultEl.innerHTML = data.drinks[0].strInstructions;
+                    const recipeData = encodeURIComponent(data.drinks[0].strInstructions);
+                    window.location.href = `results.html`;
                 } else {
                     console.error("No results element found");
                 }
@@ -26,6 +32,3 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
-
-// const recipeData = encodeURIComponent(data.drinks[0].strInstructions);
-//                 window.location.href = `recipe.html?recipe=${recipeData}`;
